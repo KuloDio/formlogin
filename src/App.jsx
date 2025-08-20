634b478f84a9b9ba4510430072b29937770a27a8
KuloDio <diomaulana701@gmail.com>
2025-08-20 15:15:50 +0700

    Penambahan Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Login from './pages/Login.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Register from './pages/Register.jsx';
+import LandingPage from './pages/LandingPage.jsx'
 import { Routes, Route, Navigate } from "react-router-dom";
 
 function PrivateRoute({ children }) {
@@ -11,7 +12,7 @@ function PrivateRoute({ children }) {
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route
