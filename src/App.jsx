1a57065df4b3e4a79e1b178ed5b3e99ddfa41eea
KuloDio <diomaulana701@gmail.com>
2025-08-21 15:11:25 +0700

    Mengupdate Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Login from './pages/Login.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Register from './pages/Register.jsx';
-import LandingPage from './pages/LandingPage.jsx'
+import Home from './pages/Home.jsx'
+import DaftarResep from './pages/Daftar-Resep.jsx';
 import { Routes, Route, Navigate } from "react-router-dom";
 
 function PrivateRoute({ children }) {
@@ -12,9 +13,10 @@ function PrivateRoute({ children }) {
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/resep" element={<DaftarResep />} />
       <Route
         path="/dashboard"
         element={
