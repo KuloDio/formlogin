0a55b7b91a0636210ef69fd02ceb4beb0fca6bba
KuloDio <diomaulana701@gmail.com>
2025-08-29 14:30:08 +0700

    tes

--- src/pages/ResepPublic.jsx
+++ src/pages/ResepPublic.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar } from '../components/navbar'
-import { Box, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 
 const ResepPublic = () => {
   return (
