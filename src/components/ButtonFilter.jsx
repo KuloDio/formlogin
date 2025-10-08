966895990322290fbdd5dcd4c31780e1fd624307
KuloDio <diomaulana701@gmail.com>
2025-10-08 16:59:48 +0700

    IMPLEMENTASI DATA DARI API KE UI SESUAI CATEGORI

--- src/components/ButtonFilter.jsx
+++ src/components/ButtonFilter.jsx
@@ -156,7 +156,7 @@ const ButtonFilter = ({ setFilter }) => {
                             fontFamily: 'Poppins',
                         }}
                     >
-                        CLEAR FILTER
+                        RESET FILTER
                     </Typography>
                 </Box>
             </Button>
